38b57440e61368d732eac334a0501d1af813d525
luanbrde <127901732+luanbrde@users.noreply.github.com>
2024-04-07 13:39:28 -0300

    Add files via upload

---
+++ src/main/java/com/avansiluan/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.avansiluan.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.avansiluan.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	
+
+}
